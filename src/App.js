3f405bf37ca3cb5bc2dd5297ac382d8b80b47c98
abdullaibnhanif <abdulla.mamun100@gmail.com>
2021-10-07 20:20:14 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,129 @@
-import logo from './logo.svg';
+import { getAuth, signInWithPopup, GoogleAuthProvider, GithubAuthProvider, signOut, createUserWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
+
 import './App.css';
+import initializeAuthentication from './Firebase/firebase.initialize';
+
+initializeAuthentication();
+
+const googleProvider = new GoogleAuthProvider();
+const gitHubProvider = new GithubAuthProvider();
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState({})
+  const auth = getAuth();
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, googleProvider)
+      .then(result => {
+        const { displayName, email, photoURL } = result.user;
+        const logInUser = {
+          name: displayName,
+          email: email,
+          photo: photoURL
+        };
+        setUser(logInUser);
+      })
+    // .catch((error) => {
+    //   // Handle Errors here.
+    //   const errorCode = error.code;
+    //   const errorMessage = error.message;
+    //   // The email of the user's account used.
+    //   const email = error.email;
+    //   // The AuthCredential type that was used.
+    //   const credential = GoogleAuthProvider.credentialFromError(error);
+    //   // ...
+    // });
+  }
+
+
+
+
+  const handleGithubSignIn = () => {
+    signInWithPopup(auth, gitHubProvider)
+      .then(result => {
+        const { displayName, photoURL, email } = result.user;
+        const logInUser = {
+          name: displayName,
+          email: email,
+          photo: photoURL
+        };
+        setUser(logInUser);
+
+      })
+  }
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser({});
+      })
+  }
+
+  const handleEmailChange = e => {
+    setEmail(e.target.value);
+  }
+
+  const handlePassword = e => {
+    setPassword(e.target.value);
+  }
+  const handleRegistration = e => {
+    console.log(email, password);
+    createUserWithEmailAndPassword(auth, email, password)
+      .then(result => {
+        const user = result.user;
+        console.log(user);
+      })
+    e.preventDefault();
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="m-5">
+
+      <form onSubmit={handleRegistration}>
+        <div className="row mb-3">
+          <label htmlFor="inputEmail3" className="col-sm-2 col-form-label">Email</label>
+          <div className="col-sm-10">
+            <input onBlur={handleEmailChange} type="email" className="form-control" id="inputEmail3" required />
+          </div>
+        </div>
+        <div className="row mb-3">
+          <label htmlFor="inputPassword3" className="col-sm-2 col-form-label">Password</label>
+          <div className="col-sm-10">
+            <input onBlur={handlePassword} type="password" className="form-control" id="inputPassword3" required />
+          </div>
+        </div>
+        <div className="row mb-3">
+          <div className="col-sm-10 offset-sm-2">
+            <div className="form-check">
+              <input className="form-check-input" type="checkbox" id="gridCheck1" />
+              <label className="form-check-label" htmlFor="gridCheck1">
+                Example checkbox
+              </label>
+            </div>
+          </div>
+        </div>
+        <button type="submit" className="btn btn-primary">Sign in</button>
+      </form>
+      <br />
+      <div>-----------------------------</div>
+      <br />
+
+      {!user.name ?
+        <div>
+          <button onClick={handleGoogleSignIn}>Google Sign In</button>
+          <button onClick={handleGithubSignIn}>GitHub Sign In</button>
+        </div> :
+        <button onClick={handleSignOut}>Sign Out</button>
+      }
+      <br />
+      {
+        user.name && <div>
+          <h2>Welcome {user.name}</h2>
+          <p>I know Your Email Address: {user.email}</p>
+          <img src={user.photo} alt="" />
+        </div>
+      }
     </div>
   );
 }
---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCUgqmU-Rsy_e2d8ejDR27cpMq6PdUH3DA",
+    authDomain: "simple-firebase-authenti-fec99.firebaseapp.com",
+    projectId: "simple-firebase-authenti-fec99",
+    storageBucket: "simple-firebase-authenti-fec99.appspot.com",
+    messagingSenderId: "1024485828731",
+    appId: "1:1024485828731:web:fb94ffa70a44eb5b52dade"
+};
+
+export default firebaseConfig;
